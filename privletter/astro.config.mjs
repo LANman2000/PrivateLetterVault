20440a334887e8c2efa2f01c15a94757fe1a6bb3
Lanre Ayegbusi <layegbusi7@gmail.com>
2025-12-16 06:13:25 +0000

    Initialize with astro

---
+++ privletter/astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  site: 'http://localhost:4321',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
